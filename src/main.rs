396be7ff55907b4e9c82dd692c5ae1f83a7281a3
Jontze <42588836+jontze@users.noreply.github.com>
2025-06-15 10:36:39 +0200

    feat(db): Persist sqlite in local user store

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,24 @@ mod entity;
 
 #[tokio::main]
 async fn main() -> miette::Result<()> {
-    let db = Database::connect(std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"))
+    let db_path = if let Ok(path) = std::env::var("CLOG_DATABASE_URL") {
+        path
+    } else {
+        let user_database_dir = dirs::data_local_dir()
+            .ok_or_else(|| miette::miette!("Failed to get local data directory"))?
+            .join(env!("CARGO_PKG_NAME"));
+        std::fs::create_dir_all(&user_database_dir)
+            .map_err(|e| miette::miette!("Failed to create user database directory: {}", e))?;
+        let db_path = user_database_dir.join("db.sqlite");
+        format!(
+            "sqlite://{}?mode=rwc",
+            db_path
+                .to_str()
+                .ok_or_else(|| miette::miette!("Invalid database path"))?
+        )
+    };
+
+    let db = Database::connect(db_path)
         .await
         .map_err(|e| miette::miette!("Failed to connect to database: {}", e))?;
 
